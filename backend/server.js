e0a9d313ef1cca56f7c78d14db7ed6036e1984cf
Sushant <sushantghadge2016@gmail.com>
2025-02-09 18:14:09 +0530

    mern-messenger-backend

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const path = require("path");
+const cors = require("cors");
 
 dotenv.config();
 connectDB();
