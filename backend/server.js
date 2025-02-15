14906d6dbb582152aa44ee667e1afe6260f78cf9
Sushant <sushantghadge2016@gmail.com>
2025-02-15 11:31:56 +0530

    MERN messenger frontend

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://mern-messenger-frontend.vercel.app",
     // credentials: true,
   },
 });
