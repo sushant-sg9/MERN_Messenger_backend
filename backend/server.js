be5b31138913adbacb6a92f7de26bc560e5fe5ef
Sushant <sushantghadge2016@gmail.com>
2025-04-30 05:47:45 +0530

    new changes

--- backend/server.js
+++ backend/server.js
@@ -44,8 +44,10 @@ const io = require("socket.io")(server, {
 
 io.on("connection", (socket) => {
   console.log("Connected to socket.io");
+  
   socket.on("setup", (userData) => {
     socket.join(userData._id);
+    console.log("User setup: " + userData._id);
     socket.emit("connected");
   });
 
@@ -53,23 +55,32 @@ io.on("connection", (socket) => {
     socket.join(room);
     console.log("User Joined Room: " + room);
   });
-  socket.on("typing", (room) => socket.in(room).emit("typing"));
-  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+  
+  socket.on("typing", (room) => {
+    socket.in(room).emit("typing");
+  });
+  
+  socket.on("stop typing", (room) => {
+    socket.in(room).emit("stop typing");
+  });
 
-  socket.on("new message", (newMessageRecieved) => {
-    var chat = newMessageRecieved.chat;
+  socket.on("new message", (newMessageReceived) => {
+    var chat = newMessageReceived.chat;
 
-    if (!chat.users) return console.log("chat.users not defined");
+    if (!chat.users) {
+      console.log("chat.users not defined");
+      return;
+    }
 
     chat.users.forEach((user) => {
-      if (user._id == newMessageRecieved.sender._id) return;
+      if (user._id === newMessageReceived.sender._id) return;
 
-      socket.in(user._id).emit("message received", newMessageRecieved);
+      console.log("Sending message to user: " + user._id);
+      socket.in(user._id).emit("message recieved", newMessageReceived);
     });
   });
 
-  socket.off("setup", () => {
+  socket.on("disconnect", () => {
     console.log("USER DISCONNECTED");
-    socket.leave(userData._id);
   });
 });
